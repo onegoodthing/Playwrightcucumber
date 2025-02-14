537be157313f73c00081ee19c889eefc4e2e432c
onegoodthing <siddhantdhyani10@gmail.com>
2025-02-14 00:15:17 +0000

    14 Feb 12:15 Am adding steps and feature

---
+++ src/test/java/stepdefinitions/ExampleSteps.java
@@ -0,0 +1,29 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import com.microsoft.playwright.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleSteps {
+    private static Playwright playwright;
+    private static Browser browser;
+    private static Page page;
+
+    @Given("I open the homepage")
+    public void i_open_the_homepage() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+        page = browser.newPage();
+        page.navigate("https://example.com");
+    }
+
+    @Then("the title should be {string}")
+    public void the_title_should_be(String expectedTitle) {
+        String actualTitle = page.title();
+        assertEquals(expectedTitle, actualTitle);
+        browser.close();
+        playwright.close();
+    }
+}
---
+++ src/test/resources/features/sample.feature
@@ -0,0 +1,5 @@
+Feature: Example Feature
+
+  Scenario: Test the homepage
+    Given I open the homepage
+    Then the title should be "Example Domain"
