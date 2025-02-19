ba28297d160dafbdace766c5ed12a2338a092b29
onegoodthing <siddhantdhyani10@gmail.com>
2025-02-19 17:20:05 +0000

    Adding page object class with locators

---
+++ src/test/java/pages/WealthManagement.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class WealthManagement {
+    public static final String openColoumn="//button[@aria-label='Open the menu']";
+    public static final String wealthMangement="//a[@aria-label='Show Wealth Management submenu']";
+    public static final String wealthMangmentSubmenu="//a[@class='second-level-link'][normalize-space()='Wealth Management']";
+    public static final String bookConsultation="//a[@title='Book a consultation']";
+}
